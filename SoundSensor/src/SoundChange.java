75cdc72465fd45dd86f1cb3c4316f9c35ef636ea
eriknng <erik.ng@hotmail.com>
2016-02-08 18:44:20 +0100

    Prints to text.txt

--- SoundSensor/src/SoundChange.java
+++ SoundSensor/src/SoundChange.java
@@ -8,17 +8,29 @@
  */
 
 import com.phidgets.event.SensorChangeListener;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 import com.phidgets.event.SensorChangeEvent;
 /**
  *
  * @author Owner
  */
 public class SoundChange implements SensorChangeListener{
-    
+	
 
     public void sensorChanged(SensorChangeEvent sensorChangeEvent)
     {
-	    		System.out.println(sensorChangeEvent.getValue());
+	    		//System.out.println(sensorChangeEvent.getValue());
+	    		try (	FileWriter output = new FileWriter("text.txt", true)) {
+	    			output.write(sensorChangeEvent.getValue() + ",");
+	    			
+	    		} catch (IOException e) {
+					e.printStackTrace();
+				}
     }
     
 }
